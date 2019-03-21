8fe35a726825ec711ec07134747d2bf5283c2482
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-21 16:57:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,6 @@ end
 def find_the_cheese(foods)
    cheese_types = ["cheddar", "gouda", "camembert"]
    foods.any? do |food|
-     cheese_types.include?
+     cheese_types.include?(food)
+   end
 end
