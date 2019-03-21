ab6eb37ab413676df044bdbc18085d3105ff4606
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-21 16:28:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,8 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(planeteer_calls)
- planeteer_calls.collect do |call|
-   "#{call[0].upcase}!"
+ planeteer_calls.collect! do |call|
+   call.capitalize
  end
 end
 
