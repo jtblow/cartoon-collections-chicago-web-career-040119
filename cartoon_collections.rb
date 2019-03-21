41c48c56dceb0c4478361846338fc0c4fb2cb0e8
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-21 16:35:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(array)
-  array.include?
+  array.include?(word.length > 4)
+end
 end
 
 def find_the_cheese# code an argument here
